700fa920af913aa15bf3144831f17aa63085a2a9
Gustavonvp <gustavonvp@gmail.com>
2021-12-18 20:54:26 -0300

    images

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
+  @Input() data = [];
   constructor() { }
 
   ngOnInit() {
--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewsHomeComponent implements OnInit {
 
+  stats = [
+    {
+        value: 22, label: '# of Users'
+    },
+    {
+        value:900, label: 'Revenue'
+    },
+    {
+        value:50, label: 'Reviews'
+    },
+  ];
+
+  items = [
+    {
+      image: '../../assets/couch.jpeg',
+      title: 'Counch',
+      description: 'This is afantastic couch to sit on'
+    },
+    
+    {
+      image: '../../assets/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a great dresser to put in on!'
+    }
+  ]
+
+  
+
   constructor() { }
 
   ngOnInit() {
